54c0e4d8016dff675a727adcab7b79aae7efa9d1
usbpc102 <spieler3001@googlemail.com>
2013-10-22 18:34:27 +0200

    added a check if a player is looking at a block when rightclicking MyTestItem.

--- source/usbpc102/mod/rfc/item/MyFirstItem.java
+++ source/usbpc102/mod/rfc/item/MyFirstItem.java
@@ -1,7 +1,10 @@
 package usbpc102.mod.rfc.item;
 
 import net.minecraft.creativetab.CreativeTabs;
+import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemStack;
+import net.minecraft.world.World;
 
 public class MyFirstItem extends Item {
 
@@ -11,5 +14,19 @@ public class MyFirstItem extends Item {
 		setCreativeTab(CreativeTabs.tabMisc);
 		setUnlocalizedName("myFirstItem");
 	}
+	
+	
+
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+    	 if(par3EntityPlayer.rayTrace(4.0, 1)!=null) {
+             if(par2World.isRemote) {
+            	 System.out.println("Player is pointing at a Block");
+             }
+    	 } else {
+    		 System.out.println("Player is not Pointing at a Block");
+     }
+		return par1ItemStack;
+    }
+
 
 }
